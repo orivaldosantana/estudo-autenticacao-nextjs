faaedad322dfcacf1c208fb7edbf650820454bfe
Orivaldo Santana <orivaldo@gmail.com>
2023-02-15 14:26:08 -0300

    início do componente de contexto de autenticação

---
+++ auth-t1/contexts/AuthContext.js
@@ -0,0 +1,16 @@
+import { createContext } from "react";
+
+export const AuthContext = createContext({})
+
+export function AuthProvider({ children }){
+   const isAuthenticated = false; 
+
+   async function signIn() {
+      
+   }
+   return (
+      <AuthContext.Provider value={{ isAuthenticated }} >
+         {children}
+      </AuthContext.Provider>
+   )
+}
