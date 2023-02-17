f1728a5ff5f848058be9bd82670d37cdc65164d3
Orivaldo Santana <orivaldo@gmail.com>
2023-02-17 14:54:09 -0300

    pagina inicial de dashboard

--- auth-t1/contexts/AuthContext.js
+++ auth-t1/contexts/AuthContext.js
@@ -1,5 +1,6 @@
 import { createContext, useState } from "react"
 import { setCookie } from "nookies"
+import Router from "next/router"
 import { http } from "@/utils/http"
 
 export const AuthContext = createContext({})
@@ -12,15 +13,17 @@ export function AuthProvider({ children }) {
     console.log("signIn " + email + " " + password)
     http.post("/auth/signin", { email, password }).then((res) => {
       const userRes = res.data
-      console.log(userRes)
-      const token = "xxxx"
+      console.log(userRes.access_token)
+      const token = userRes.access_token
       setCookie(undefined, "nextautht1.token", token, {
         maxAge: 60 * 30 * 1, //30 min
       })
     })
+
+    Router.push("/dashboard")
   }
   return (
-    <AuthContext.Provider value={{ isAuthenticated, signIn }}>
+    <AuthContext.Provider value={{ isAuthenticated, signIn, user }}>
       {children}
     </AuthContext.Provider>
   )
